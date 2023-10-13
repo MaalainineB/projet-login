038cf65c5c659b15be9ecfc40278e65162ba8d1e
MaalainineB <bouchalga60@gmail.com>
2023-10-13 11:14:28 +0200

    forms css modified

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -13,6 +13,7 @@ export class UserService {
 
   login(formData: any): Observable<any> {
     let option:any
+    //option {...option, responseType: 'text' } est un objet  qui contient les options de la requette, la reponse de la requette sera traité comme un text.
     return this.http.post<any>('http://localhost:8080/auth/generateToken', formData, {...option, responseType: 'text' })
   }
 }
